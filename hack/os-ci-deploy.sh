8c3670f356242a11eeeef16d49b8e2a652a55dfa
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-12-04 14:23:24 +0100

    Create os-ci-builder
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

---
+++ hack/os-ci-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export GIMME_GO_VERSION=1.12.8
+export GOPATH="/go"
+export GOBIN="/usr/bin"
+source /etc/profile.d/gimme.sh
+
+export DOCKER_PREFIX='https://registry.svc.ci.openshift.org/kubevirt/kubevirt'
+export DOCKER_TAG="$( echo "${JOB_NAME}-${BUILD_ID}" | md5sum -t | cut -c 1-8 )"
+export KUBEVIRT_PROVIDER=external
+
+echo "building and pushing images"
+bash -x ./hack/bazel-push-images.sh
+
+echo "creating manifests"
+bash -x ./hack/build-manifests.sh
+
+echo "deploying"
+bash -x ./hack/cluster-deploy.sh
+
+echo "testing"
+bash -x ./hack/build-func-tests.sh"
+bash -x ./hack/functests.sh
